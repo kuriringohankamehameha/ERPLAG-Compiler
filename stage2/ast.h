22d821ed2267d24f0d0d67050c08b7d84fa51746
kuriringohankamehameha <42909864+kuriringohankamehameha@users.noreply.github.com>
2020-04-12 17:45:16 +0530

    Update ast.h

--- stage2/ast.h
+++ stage2/ast.h
@@ -3,6 +3,7 @@
 
 #include "common.h"
 #include "lexerDef.h"
+#include "parserDef.h"
 
 typedef struct ASTNode {
     // The Abstract Syntax Tree Node
@@ -11,5 +12,13 @@ typedef struct ASTNode {
     struct ASTNode* parent, **children; // Pointers to the parent and children
     int num_children;
 }ASTNode;
+
+ASTNode* make_ASTNode(ASTNode* child, term token_type);
+void add_ASTChild(ASTNode* parent, ASTNode* child);
+ASTNode* make_ASTLeaf(ASTNode* parent, Token token);
+void generate_AST(TreeNode* root);
+void print_AST(ASTNode* root);
+void print_AST_without_parent(ASTNode* parent);
+void free_AST(ASTNode* root);
 
 #endif
